cb4631db2e35a7b83df4ceb7010d7fb62efda224
sureshmurali710 <54928199+sureshmurali710@users.noreply.github.com>
2021-05-02 20:39:47 -0400

    Update Problem1.java

---
+++ Problem1.java
@@ -0,0 +1,23 @@
+Time Complexity: o(n)
+
+class Solution {
+    public int[] find_target(int [] nums, int t){
+        if(nums == null || nums.length == 0) return 0;
+        t = 9;
+
+        HashMap<Integer, Integer> map = new HashMap<>(); //initialize
+
+        for(int i=0; i < n.length; i++) {
+            if (map.get(i) != map) {
+                map.put(n[i], i);
+            }
+        }
+
+        for(int i=0; i < n.length; i++)    {
+            int complement = t-n[i];
+            if(map.containsKey(complement) && map.get(complement) != i){
+                return new int[](i, map.get(complement));
+            }
+
+    }
+}
